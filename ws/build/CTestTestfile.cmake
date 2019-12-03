405718763914e43d16f675b141850fe2dec240f7
Zack Pelster <pelsterz@oregonstate.edu>
2019-12-03 22:14:17 +0000

    Added i2c test files and workspace

---
+++ ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/project_theia/ws/src
+# Build directory: /home/ubuntu/project_theia/ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
