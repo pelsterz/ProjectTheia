2ea15e0a820866e2475907a267ccb946fa208413
Zack Pelster <pelsterz@oregonstate.edu>
2019-12-09 01:55:10 +0000

    Technical demo considered complete, slight bug that servo can jitter in rare instances

--- ws/src/i2c_communication/src/process_i2c.py
+++ ws/src/i2c_communication/src/process_i2c.py
@@ -2,17 +2,24 @@
 
 import time
 import rospy
+import math
 
 from i2c_communication.msg import i2c
 
+def arduino_map(x, in_min, in_max, out_min, out_max):
+    return (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
+
 def callback(raw_msg):
-    print "test"
+    print "Process: " + str(raw_msg.value)
+    
     msg = i2c()
     msg.header.frame_id = 'angle'
     msg.header.stamp = rospy.Time.now()
 
     # Map the raw value from the pot to an angle for the servo
-    msg.value = 180 * (raw_msg.value/1023)
+    msg.value = arduino_map(raw_msg.value, 1023, 0, 0, 180)
+    
+    print "Result: " + str(msg.value)
 
     pub.publish(msg)
 
--- ws/src/i2c_communication/src/read_write_i2c.py
+++ ws/src/i2c_communication/src/read_write_i2c.py
@@ -1,32 +1,24 @@
 #!/usr/bin/env python2
 
-import smbus
+import subprocess
 import time
 import rospy
+
+from smbus2 import SMBus, i2c_msg
 
 from i2c_communication.msg import i2c
 
 bus = None
 address = 0x04
 
+class Send:
+    def __init__(self):
+        self.value = 0
+
 def callback(angle_msg):
-    bus.write_byte(address, angle_msg.value)
-
-def timer(event):
-    # Set up message
-    msg = i2c()
-    msg.header.frame_id = 'pot'
-    
-    # Add values to message
-    msg.header.stamp = event.current_real
-    msg.value = bus.read_word_data(address, 0) & 0x03FF
-
-    pub.publish(msg)
+    message.value = angle_msg.value
 
 if __name__ == "__main__":
-    # Initialize i2c
-    bus = smbus.SMBus(1)
-
     # Initialize the node
     rospy.init_node('read_write_i2c', log_level=rospy.DEBUG)
 
@@ -36,8 +28,34 @@ if __name__ == "__main__":
     # Setup subscriber
     sub = rospy.Subscriber('/servo_angle',i2c,callback)
 
-    # Setup timer
-    rospy.Timer(rospy.Duration(0.01), timer)
+    message = Send()
 
-    #Turn control over to ROS
-    rospy.spin()
+    msg = i2c()
+    msg.header.frame_id = 'pot'
+
+    rate = rospy.Rate(100) # Set rate
+
+    # Loop until ROS is shutdown
+    while not rospy.is_shutdown():
+        # Add values to message
+        msg.header.stamp = rospy.Time.now()
+        
+        write = i2c_msg.write(address, [message.value])
+        read = i2c_msg.read(address, 2)
+        with SMBus(1) as bus:
+            try:
+                bus.i2c_rdwr(write, read)
+                value = 0
+                i = 0
+                for val in read:
+                    value += val << 8*i
+                    i += 1
+
+                msg.value = value & 0x03FF
+
+            except IOError:
+                # Handle IOError (Try sending message again next loop)
+                subprocess.call(['i2cdetect', '-y', '1'])
+
+        pub.publish(msg)
+        rate.sleep()
